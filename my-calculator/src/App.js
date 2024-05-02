a322985767d8fc9e2587150eff907c5da5a2287d
tizarus <50199109+tizarus@users.noreply.github.com>
2024-05-02 01:03:07 -0500

    added styles.css

--- my-calculator/src/App.js
+++ my-calculator/src/App.js
@@ -1,6 +1,8 @@
+import "./styles.css"
+
 function App() {
   return(
-    <div className="calulator-grid">
+    <div className="calculator-grid">
       <div className="output">
         <div className="previous-operand"></div>
         <div className="current-operand"></div>        

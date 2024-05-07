334c9aa6f086a97078d3031a073682c4a8f145a2
tizarus <50199109+tizarus@users.noreply.github.com>
2024-05-07 14:00:29 -0500

    AC action updated

--- my-calculator/src/App.js
+++ my-calculator/src/App.js
@@ -20,6 +20,9 @@ function reducer(state, { type, payload }) {
         ...state,
         currentOperand: `${state.currentOperand || ""}${payload.digit}`
       }
+      case CHOOSE_OPERATION:
+        if (state.currentOperand == null && state.previousOperand == null)
+        return state
       case ACTIONS.CLEAR:
         return {}
   }
